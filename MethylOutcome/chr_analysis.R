926a2ccb51d573a909ca9643fec0f1825635162e
Jessica Murphy <jessica.murphy@ucdenver.edu>
2021-03-08 15:12:07 -0700

    upload files with new capitalizations

---
+++ MethylOutcome/chr_analysis.R
@@ -0,0 +1,24 @@
+############################################################
+##  Women First Trial: Guatemala Methylation Analysis         
+##  Written by Jessica Murphy 
+##  Last edited on August 30, 2020
+##  This script performs the methylation analysis for chromosome mychrom.
+##  Please send any questions to jessica.murphy@ucdenver.edu
+############################################################
+
+# specify chromosome
+j = mychrom
+
+setwd("/nfs/storage/math/gross-s2/projects/guatemala/Guatemala-omics/Methylation/EWAS/data/")
+
+# link to analysis function
+source("/nfs/storage/math/gross-s2/projects/guatemala/Guatemala-omics/Methylation/EWAS/analysis/analysis_function.R")
+
+# read in chromosome data (samples by sites)
+file.name = paste0("Chr", j, "_data4analysis.rda")
+load(file.name)
+
+setwd("/nfs/storage/math/gross-s2/projects/guatemala/Guatemala-omics/Methylation/EWAS/results/")
+
+# perform analysis (call analysis function)
+analysis(combined, j)
